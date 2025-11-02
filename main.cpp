72e10e0d9502e486c9b71e660904b02c4e328957
Shreya Singh <shreyasingh@Shreyas-MacBook-Air.local>
2025-11-03 00:11:08 +0530

    Raw and Canonical mode
    enable raw mode, turn off echo
    
    turn off canonical mode
    
    Read one char at a time to either print it or not

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 #include <unistd.h>  // access to POSIX opsys API (read, write, fork, exec...)
 #include <sys/wait.h>
 #include <fstream> // for ofstream
+#include <ctype.h> // for iscntrl
 
 struct termios orig_termios;
 void disableRawMode()
@@ -17,17 +18,17 @@ void disableRawMode()
 void enableRawMode()
 {
     tcgetattr(STDIN_FILENO, &orig_termios);
-    atexit(disableRawMode);
 
-    struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO);
+    struct termios raw = orig_termios;// to turn of printing... typing on terminal
+    raw.c_lflag &= ~(ECHO | ICANON);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main()
 {
-    string s;
+    enableRawMode();
+    char c;
 
     // opening file for storing
     ofstream myfile;
@@ -35,15 +36,54 @@ int main()
 
     for (;;)
     {
-        cout << "\nshreyashell> ";
+        string s = "";
+        cout << "shreyashell> " << flush;
 
-        std::getline(cin, s);
+        while (read(STDIN_FILENO, &c, 1))
+        { // Read one character at a time, exit on 'q'
+            if (c == '\n')
+            {
+                cout << '\n';
+                break;
+            }
+            if (iscntrl(c))
+            {
+                char s[10];
+                if (read(STDIN_FILENO, s, 3))
+                {
+                    if (s[1] == 'A')
+                        cout << "Up Arrow Pressed\n" ;
+                    else if (s[1] == 'B')
+                        cout << "Down Arrow Pressed\n";
+                    else if (s[1] == 'C')
+                        cout << "Right Arrow Pressed\n";
+                    else if (s[1] == 'D')
+                        cout << "Left Arrow Pressed\n";
+                        break;
+                    // else
+                    //     cout << s[1] << "\n";
+                }
+                else
+                    cout << "Read failed\n";
+            }
+            else
+            {
+                //raw.c_lflag |= ECHO;
+                s += c;
+                cout << c << flush;
+            }
+            // printf("%c", c);
+            // fflush();
+        }
 
         // storing
         myfile << s << endl;
 
         if (s == "exit")
+            {
+            atexit(disableRawMode);
             break;
+        }
         else if (s == "history")
         {
             string cmd;
@@ -76,7 +116,7 @@ int main()
             if (pid == 0)
             {
                 execvp(args[0], args.data());
-                cout << "invalid command" << endl; // If exec fails
+                cout << "invalid command\n"; // If exec fails
                 exit(1);
             }
             else
